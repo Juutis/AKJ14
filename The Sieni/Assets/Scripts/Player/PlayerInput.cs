18f27a65b2aeab40e6145240e2ce92faa70aee1b
Juutis <>
2022-02-26 11:32:14 +0200

    Player Container

--- The Sieni/Assets/Scripts/Player/PlayerInput.cs
+++ The Sieni/Assets/Scripts/Player/PlayerInput.cs
@@ -13,10 +13,21 @@ public class PlayerInput : MonoBehaviour
     [SerializeField]
     private bool triggerInputResetForDebugPurposes = false;
 
+    private Vector2 input;
+    private Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate() {
+        var up = Camera.main.transform.up.normalized;
+        var right = Camera.main.transform.right.normalized;
+        rb.velocity = (input.x * right + input.y * up) * moveSpeed;
         
+        Debug.Log(rb.velocity);
     }
 
     // Update is called once per frame
@@ -24,15 +35,14 @@ public class PlayerInput : MonoBehaviour
     {
         var horizontalInput = RemappableInput.Main.GetHorizontal();
         var verticalInput = RemappableInput.Main.GetVertical();
-        var input = new Vector2(horizontalInput, verticalInput);
+        input = new Vector2(horizontalInput, verticalInput);
         if (input.magnitude >= 1.0f) input.Normalize();
-
-        transform.position += (Vector3)input * Time.deltaTime * moveSpeed;
 
         debugStuff();
     }
 
-    private void debugStuff() {
+    private void debugStuff()
+    {
         if (triggerInputResetForDebugPurposes)
         {
             triggerInputResetForDebugPurposes = false;
